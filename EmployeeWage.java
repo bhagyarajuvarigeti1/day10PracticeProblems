2744e62c8ca34f56de848569baecafb237d949eb
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-07-02 16:07:14 +0530

    UC2 branch of employee wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,11 +5,14 @@ public class EmployeeWage {
         Scanner in = new Scanner(System.in);
         System.out.println("Welcome to Employee Wage Computation");
         int status = (int) (Math.random()%3);// status of the employee
+        int Wage_per_Hour = 20,Full_Day_Hour = 8,Total_Wage = 0;
 
         if (status == 0){
             System.out.println("Employee is absent");
         } else {
+            Total_Wage = Wage_per_Hour * Full_Day_Hour;
             System.out.println("Employee is present");
+            System.out.println(Total_Wage);
         }
     }
 }
