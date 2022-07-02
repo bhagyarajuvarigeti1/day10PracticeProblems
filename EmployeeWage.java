9e685b2e85a84501dcd3a4dc26a0817102daf572
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-07-02 16:03:54 +0530

    Employee wage Uc1 branch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,5 +4,12 @@ public class EmployeeWage {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Welcome to Employee Wage Computation");
+        int status = (int) (Math.random()%3);// status of the employee
+
+        if (status == 0){
+            System.out.println("Employee is absent");
+        } else {
+            System.out.println("Employee is present");
+        }
     }
 }
