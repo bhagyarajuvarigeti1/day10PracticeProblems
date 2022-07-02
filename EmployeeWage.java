532a01924e9225a4a1d0333c6e039af707a89383
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-07-02 16:37:40 +0530

    UC8 branch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,7 +6,10 @@ public class EmployeeWage {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Welcome to Employee Wage Computation");
-        int
+        String companyName = in.nextLine();
+        
+        employeeWage();
+
 
 
     }
