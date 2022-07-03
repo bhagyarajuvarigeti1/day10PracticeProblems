77b4dc9849cd0121bbd87520bf1b5bd86f3b0127
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-07-03 18:34:53 +0530

    manage mulitple companies using Interface approach branch UC11

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,9 +1,7 @@
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
-public class EmployeeWage {
+public class EmployeeWage implements EmployeeWageBuilder {
     public static int day = 20, Hours = 100, Wage_per_Hour = 20, Full_Day_Hour = 8, Part_Time_Hour = 8, Total_Wage = 0;
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -22,26 +20,40 @@ public class EmployeeWage {
         }
 
     }
-    public static void employeeWage(String companyName) {
+
+
+    @Override
+    public void employeeWage(String companyName) {
         int status;
         while (day != 0 && Hours != 0) {
             status = (int) (Math.random()%3);// status of the employee
-            switch (status) {
-                case 0: {
-                    System.out.println("Employee is absent");
-                }
-                case 1: {
-                    System.out.println("Part time Hour");
-                    Total_Wage = Wage_per_Hour * Part_Time_Hour;
-                }
-                default: {
-                    Total_Wage = Wage_per_Hour * Full_Day_Hour;
-                    System.out.println("Employee is present");
-                    System.out.println(Total_Wage);
-                }
-            }
+            employeeStatus(status);
             day--;Hours--;
         }
         System.out.println(companyName + " " + Total_Wage);
     }
+
+    @Override
+    public void employeeStatus(int status) {
+        switch (status) {
+            case 0: {
+                System.out.println("Employee is absent");
+            }
+            case 1: {
+                System.out.println("Part time Hour");
+                Total_Wage = Wage_per_Hour * Part_Time_Hour;
+            }
+            default: {
+                Total_Wage = Wage_per_Hour * Full_Day_Hour;
+                System.out.println("Employee is present");
+                System.out.println(Total_Wage);
+            }
+        }
+    }
+}
+
+
+interface EmployeeWageBuilder{
+    void employeeWage(String name);
+    void employeeStatus(int status);
 }
